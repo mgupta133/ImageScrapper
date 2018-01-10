e79501f56dd5fc0876cbff1e08280d5ca3119b3c
Pradeep7kumar <pradeepahwr@gmail.com>
2018-01-10 12:55:54 +0530

    index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('scrapper', { title: 'Express' });
+  res.render('scrapper.html', { title: 'Express' });
 });
 
 module.exports = router;
